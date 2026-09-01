479e3979428d870e0b92fc3650ff376056174c70
GuiBRA985 <gui@bento.host>
2026-09-01 18:58:33 -0400

    Add files via upload

---
+++ js/data/cidades.js
@@ -0,0 +1,13 @@
+const cidades = [
+
+{
+    id:1,
+    tipo:"cidade",
+    nome:"Poconé",
+    km:0,
+    lat:-16.2567,
+    lng:-56.6228,
+    descricao:"Portal de entrada da Transpantaneira."
+}
+
+];
---
+++ js/data/destinos.js
@@ -0,0 +1,52 @@
+const destinos = [
+
+    {
+        id: 4,
+        tipo: "destino",
+        nome: "Porto Jofre",
+        km: 145,
+        lat: -17.36460,
+        lng: -56.77490,
+
+        foto: "img/destinos/porto-jofre.jpg",
+        site: "https://www.google.com/search?q=Porto+Jofre+Pantanal",
+
+        roteiroJaguar: "Destino final da Expedição Jaguar",
+        diasPacote: 3,
+
+        descricao:
+            "Destino final da Expedição Jaguar. Porto Jofre é um dos lugares mais famosos do mundo para observação da onça-pintada em vida livre, especialmente nos passeios de barco pelos rios e corixos do Pantanal Norte.",
+
+        destaque:
+            "O ponto alto da viagem: a busca pela onça-pintada em liberdade.",
+
+        melhorPara: [
+            "🐆 Onça-pintada",
+            "🚤 Safári fluvial",
+            "📸 Fotografia de vida selvagem",
+            "🌅 Pôr do sol no rio"
+        ],
+
+        fauna: [
+            "Onça-pintada",
+            "Ariranha",
+            "Jacaré-do-pantanal",
+            "Capivara",
+            "Tuiuiú",
+            "Garças",
+            "Martim-pescador"
+        ],
+
+        atividades: [
+            "Passeios de barco",
+            "Observação de onças",
+            "Fotografia de fauna",
+            "Amanhecer no rio",
+            "Pôr do sol no Pantanal",
+            "Observação de aves aquáticas"
+        ],
+
+        tempoIdeal: "3 noites no pacote promocional"
+    }
+
+];
